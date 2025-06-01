5ed2f17f6fae1ac99d9cc92fd9bd6324d296eb73
Muhammad Afrasyab <afrasyab.dev@gmail.com>
2025-06-01 20:32:40 +0500

    Fixed image import issue

--- src/assets/images/Index.ts
+++ src/assets/images/Index.ts
@@ -2,7 +2,7 @@ import Accuracy from "../LandingImages/SVGs/accuracy.svg";
 import Insights from "../LandingImages/SVGs/insights.svg";
 import Speed from "../LandingImages/SVGs/speed.svg";
 import WinRate from "../LandingImages/SVGs/winrate.svg";
-import HowItWorks from "../LandingImages/SVGs/howitworks.svg";
+import HowItWorks from "../LandingImages/SVGs/howItWorks.svg";
 import Ellipse from "../LandingImages/PNGs/Ellipse.png";
 
 export const LandingImages = {
